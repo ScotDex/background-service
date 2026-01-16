3683fbb9b55a889d14e2a28ca33932d48119fb47
ScottishDex <35346954+ScotDex@users.noreply.github.com>
2026-01-16 12:53:21 +0000

    implementing image proxy

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const { pipeline } = require('node:stream/promises');
+const { error } = require('node:console');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
+
+const CACHE_DIR = path.join(__dirname, 'cache', 'renders');
+if (!fs.existsSync(CACHE_DIR)) fs.mkdirSync(CACHE_DIR, { recursive: true });
 
 // 1. Load your credentials
 const sslOptions = {
@@ -29,6 +35,50 @@ app.get('/random', (req, res) => {
     });
 });
 
+const pendingRequests = new Map();
+
+app.get ('/render/ship/:typeId', async (req, res) => {
+    const { typeId } = req.params;
+    const localPath = path.join(CACHE_DIR, `${typeId}.png`);
+
+    if (fs.existsSync(localPath)) {
+        res.set('Cache-Control', 'public, max-age=31536000, immutable'); 
+        return res.sendFile(localPath);
+    }
+
+    if (pendingRequests.has(typeId)) {
+        await pendingRequests.get(typeId);
+        return res.sendFile(localPath);
+    }
+
+    const fetchPromise = (async () => {
+        try {
+            const ccpUrl = `https://images.evetech.net/types/${typeId}/render?size=64`;
+            const response = await axios ({ url: ccpUrl, responseType: 'stream' });
+
+            if (response.status !== 200) throw new error ('CCP Server Error');
+
+            await pipeline(response.data, fs.createWriteStream(localPath));
+
+        } catch (err) {
+            console.Error (`Failed to fetch ship ${typeId}`);
+            throw err
+        } finally {
+            pendingRequests.delete(typeId);
+        }
+    })();
+
+    pendingRequests.set(typeId, fetchPromise);
+
+    try {
+        await fetchPromise;
+        res.set('Cache-Control', 'public, max-age=31536000, immutable');
+        res.sendFile(localPath);
+    } catch (err) {
+        res.set('Cache-Control', 'no-store'); // Don't cache failures
+        res.status(404).json({ error: "Ship render unavailable" });
+    }
+});
 // 2. Start the HTTPS server
 https.createServer(sslOptions, app).listen(2053, '0.0.0.0', () => {
     console.log("🔒 Nebula Provider Online via HTTPS on Port 2053");
